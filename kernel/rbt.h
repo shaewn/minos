1305d7dec866b0afb5029c0dfae9f8d05a7df787
Sean McCain <seanm6407@gmail.com>
2025-08-06 10:15:48 -0500

    update rbt

--- kernel/rbt.h
+++ kernel/rbt.h
@@ -1,12 +1,23 @@
 #ifndef KERNEL_RBT_H_
 #define KERNEL_RBT_H_
 
+#include "types.h"
+
+#define get_blackness(node_ptr) ((node_ptr)->parent_and_color & 0x3)
+#define set_blackness(node_ptr, blackness)                                                         \
+    ((node_ptr)->parent_and_color =                                                                \
+         ((node_ptr)->parent_and_color & ~(uintptr_t)0x3) | ((blackness) & 0x3))
+
+#define get_parent(node_ptr) ((struct rb_node *)((node_ptr)->parent_and_color & ~(uintptr_t)0x3))
+#define set_parent(node_ptr, parent_ptr)                                                           \
+    ((node_ptr)->parent_and_color = ((node_ptr)->parent_and_color & 0x3) | ((uintptr_t)(parent_ptr)))
+
 // To be embedded in the structures of your desire.
-// Use CONTAINER_OF to access your structure
+// Use CONTAINER_OF to access your structure.
+// Don't directly modify anything.
 struct rb_node {
-    struct rb_node *parent;
+    uintptr_t parent_and_color;
     struct rb_node *left, *right;
-    int blackness;
 };
 
 void rb_link_node(struct rb_node *node, struct rb_node *parent, struct rb_node **pnode);
